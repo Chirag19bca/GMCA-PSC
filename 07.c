6a02513a231efc64c852630dde524e2ee48c1e3a
Chirag <119275895+Chirag19bca@users.noreply.github.com>
2025-08-21 12:27:27 +0530

    Add files via upload

--- 07.c
+++ 07.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 int main()
 {
+    int no;
+    printf("\n Enter No: ");
+    scanf("%d",&no);
+
+    if(no>0)
+    {
+        printf("\n %d number is Positive.",no);
+    }
+    else if(no<0)
+    {
+        printf("\n %d number is Negative.",no);
+    }
+    else
+    {
+        printf("\n %d number is Zero.",no);
+    }
     return 0;
 }
