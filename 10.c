ec88de85d0848844cd1061662c3a71af032780c0
Chirag <119275895+Chirag19bca@users.noreply.github.com>
2025-08-25 17:07:17 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -7,7 +7,21 @@ int main()
 
     if(ch >= 'A' && ch <= 'Z')
     {
-        printf("\n ");
+        printf("\n character is capital letter.");
     }
+    else if(ch >= 'a' && ch <= 'z')
+    {
+        printf("\n character is small letter.");
+    }
+    else if(ch >= '0' && ch <= '9')
+    {
+        printf("\n character is digit.");
+        
+    }
+    else
+    {
+        printf("\n character is special character.");
+    }
+    
     return 0;
 }
