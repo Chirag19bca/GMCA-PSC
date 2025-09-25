26c0bde8f040567a4f6df0f54cd0aee2a2592bc0
Chirag <119275895+Chirag19bca@users.noreply.github.com>
2025-09-25 15:24:55 +0530

    Add files via upload

--- 08.c
+++ 08.c
@@ -1,11 +1,22 @@
 /*08.Write a c program to prepare pay slip using
 following data. Da = 10% of basic, Hra = 7.50%
-of basic, Ma = 300, Pf = 12.50% of basic, Gross
+of basic, Ma = 300, Pf = 12.50% of basic, Gross 
 = basic + Da + Hra + Ma, Nt = Gross – Pf*/
 #include <stdio.h>
 int main()
 {
     float basic;
-    float Da,Hra,Pf,Ma=300,Nf,gross_basic;   
+    float Da,Hra,Pf,Ma,Nf,gross_basic;
+    printf("Enter basic salary: ");
+    scanf("%f",&basic);
+    Da=basic * 0.10;
+    Ma=300;
+    Hra=basic * 0.075;
+    gross_basic=Da+basic+Hra+Ma;
+
+    printf("\n Gross Salary: %.2f",gross_basic);
+    Pf=basic * 0.125;
+    Nf=gross_basic-Pf;
+    printf("\n Net salary: %.2f",Nf);
     return 0;
 }
--- 12.c
+++ 12.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+    int sum,n,i;
+    sum=0;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        sum=sum + pow(i,2);
+    }
+    printf("Sum of series: %d",sum);
     return 0;
 }
