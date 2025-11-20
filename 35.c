ad1d00751abbb1a3dc1ccb4484d40c60e9767cfb
Chirag <119275895+Chirag19bca@users.noreply.github.com>
2025-11-20 15:13:47 +0530

    Add files via upload

--- 35.c
+++ 35.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+void Toupper(char str[])
+{
+    printf("%s", strupr(str));
+}
 int main()
 {
+    char str[100];
+    printf("\n enter string: ");
+    gets(str);
+    Toupper(str);
     return 0;
 }
