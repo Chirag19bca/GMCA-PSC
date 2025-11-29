b0ec6b23dd81b14510224965a2ba2eb071083780
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2025-11-29 19:31:59 +0530

    updates

--- 34.c
+++ 34.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-int main()
+float factorial(float n)
 {
+    if(n <= 0)
+    {
+        printf("\n INVAILD NUMBER it's should be positive number");
+        return 0;
+    }
+    else
+    {
+        return n*factorial(n-1);
+    }
+}
+float main()
+{
+    float n;
+    printf("Enter Number: ");
+    scanf("%f",&n);
+    printf("\n factorial of %.2f: %.2f",n,factorial(n));
     return 0;
 }
--- 36.c
+++ 36.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+struct student
+{
+    int Rollno;
+    char Name[100];
+};
 int main()
 {
+    struct student s1;
+    printf("Enter Student RollNo: ");
+    scanf("%d",&s1.Rollno);
+    printf("Enter Student Name: ");
+    scanf("%s",s1.Name);
+    printf("Student RollNo: %d \n",s1.Rollno);
+    printf("Student RollNo: %s",s1.Name);
     return 0;
 }
